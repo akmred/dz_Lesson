dd60c4a58fbe7252d22c8e81f9ca202404246307
Anton Korzhov <akmred@gmail.com>
2020-03-15 20:16:12 +0300

    1. В погодном приложении создайте еще один экран — экран для выбора города. На экране выбора города пользователь должен выбрать интересующий его город и по нажатию на кнопку переходить на уже сделанный вами экран отображения температуры. Как будут связаны и как будут взаимодействовать экраны выбора города и информации о температуре, мы узнаем из следующих занятий. Пока достаточно нарисовать экраны. 2. Экран выбора города может включать TextView для ввода города, список наиболее популярных городов и CheckBox для дополнительной информации: скорость ветра и давление. Используйте подходящие групповые вью для обоих экранов.

--- app/src/main/java/com/example/worklesson/MainActivity.java
+++ app/src/main/java/com/example/worklesson/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Switch;
 public class MainActivity extends AppCompatActivity {
     Switch blackTheme;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +22,34 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     if (isChecked){
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        setTheme(R.style.Theme_AppCompat_DayNight);
                     }
                 }
             });
         }
     }
 
+    public void onClickCustomization(View view) {
+
+        setContentView(R.layout.activity_customization);
+
+    }
+
+
+    public void onClickBackMainActivity(View view) {
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBackCustomizeActivity(View view) {
+
+        setContentView(R.layout.activity_customization);
+
+    }
+
+    public void onClickEditTown(View view) {
+
+        setContentView(R.layout.avtivity_choice_town);
+
+    }
 }
