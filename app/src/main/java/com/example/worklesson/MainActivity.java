59a4ccd02c5e236a4734f009367c17f48ef7b18a
Anton Korzhov <akmred@gmail.com>
2020-03-17 21:05:13 +0300

    1. Используйте Toast для вывода последовательности вызовов методов жизненного цикла при различных действиях пользователей. При запуске приложения, при повороте экрана, при нажатии кнопки “Домой”, восстановлении приложения, кнопки возврат, при удалении приложения из памяти… 2. Используйте Log.d для вывода этих же данных в консоль.

--- app/src/main/java/com/example/worklesson/MainActivity.java
+++ app/src/main/java/com/example/worklesson/MainActivity.java
@@ -4,24 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Switch blackTheme;
+    public static final boolean isDebug = true;
+    public final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String instanceState;
+
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+        } else {
+            instanceState = "Повторный запуск!";
+        }
+
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()",
+                Toast.LENGTH_SHORT).show();
+        MyLogger("onCreate");
+
         blackTheme = findViewById(R.id.idBlackTheme);
-        if (blackTheme != null){
+        if (blackTheme != null) {
             blackTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked){
+                    if (isChecked) {
                         setTheme(R.style.Theme_AppCompat_DayNight);
                     }
                 }
@@ -29,10 +45,65 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onClickCustomization(View view) {
-        setContentView(R.layout.activity_customization);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        MyLogger("onStart");
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        MyLogger("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        MyLogger("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        MyLogger("onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        MyLogger("onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        MyLogger("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        MyLogger("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        MyLogger("onDestroy");
+    }
+
+                             public void onClickCustomization(View view) {
+        setContentView(R.layout.activity_customization);
+    }
 
     public void onClickBackMainActivity(View view) {
         setContentView(R.layout.activity_main);
@@ -45,4 +116,12 @@ public class MainActivity extends AppCompatActivity {
     public void onClickEditTown(View view) {
         setContentView(R.layout.avtivity_choice_town);
     }
+
+    public void MyLogger(String statement) {
+        if (isDebug) {
+            Log.v(TAG, statement);
+        }
+    }
+
 }
+
